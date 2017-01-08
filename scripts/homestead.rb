2c72c4026948d8e248eaf359d0c0f96f51a22afd
Ahmed Azaan <azaan@alliedmaldives.net>
2017-01-08 16:14:50 +0500

    rename box to homestead-56

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -13,7 +13,7 @@ class Homestead
     config.ssh.forward_agent = true
 
     # Configure The Box
-    config.vm.define settings["name"] ||= "homestead-7"
+    config.vm.define settings["name"] ||= "homestead-56"
     config.vm.box = settings["box"] ||= "laravel/homestead"
     config.vm.box_version = settings["version"] ||= "= 0.3.3"
     config.vm.hostname = settings["hostname"] ||= "homestead"
@@ -30,7 +30,7 @@ class Homestead
 
     # Configure A Few VirtualBox Settings
     config.vm.provider "virtualbox" do |vb|
-      vb.name = settings["name"] ||= "homestead-7"
+      vb.name = settings["name"] ||= "homestead-56"
       vb.customize ["modifyvm", :id, "--memory", settings["memory"] ||= "2048"]
       vb.customize ["modifyvm", :id, "--cpus", settings["cpus"] ||= "1"]
       vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
@@ -44,7 +44,7 @@ class Homestead
     # Configure A Few VMware Settings
     ["vmware_fusion", "vmware_workstation"].each do |vmware|
       config.vm.provider vmware do |v|
-        v.vmx["displayName"] = settings["name"] ||= "homestead-7"
+        v.vmx["displayName"] = settings["name"] ||= "homestead-56"
         v.vmx["memsize"] = settings["memory"] ||= 2048
         v.vmx["numvcpus"] = settings["cpus"] ||= 1
         v.vmx["guestOS"] = "ubuntu-64"
@@ -56,7 +56,7 @@ class Homestead
 
     # Configure A Few Parallels Settings
     config.vm.provider "parallels" do |v|
-      v.name = settings["name"] ||= "homestead-7"
+      v.name = settings["name"] ||= "homestead-56"
       v.update_guest_tools = true
       v.memory = settings["memory"] ||= 2048
       v.cpus = settings["cpus"] ||= 1
